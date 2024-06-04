b5e041ac0bf59c38b39567ab8dd7cc85320d28e4
Luis <luisangelarias808@gmail.com>
2024-06-04 17:36:01 -0500

    se agrega header y vista de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { product } from './shared/models/products.interface';
+
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { productsService } from '@api/products.service';
+import { HeaderComponent } from './layouts/header/header.component';
+import ProductsComponent from '@deatures/products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule ,RouterOutlet],
+  imports: [ RouterOutlet,HeaderComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   // title = 'luis-store';
-  private readonly productSvc = inject(productsService);
-  products = this.productSvc.products;
+  // private readonly productSvc = inject(productsService);
+  // products = this.productSvc.products;
 }
--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,12 +1,18 @@
+import { productsService } from '@api/products.service';
 import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
+
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export default class ProductsComponent {
-
+  private readonly productSvc = inject(productsService);
+  // products = this.productSvc.products;
 }
+
