f1f0acb12e6f4bdc2f00a893a783d84da26a3474
Luis <luisangelarias808@gmail.com>
2024-06-05 17:30:11 -0500

    se agrega carrito,falta mejorar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { productsService } from '@api/products.service';
 import { HeaderComponent } from './layouts/header/header.component';
 import ProductsComponent from '@deatures/products/products.component';
+import { CarritoComponent } from './layouts/carrito/carrito.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet,HeaderComponent,ProductsComponent],
+  imports: [ RouterOutlet,HeaderComponent,ProductsComponent,CarritoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/features/products/card/card.component.ts
+++ src/app/features/products/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { product } from '@shared/models/products.interface';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  product = input.required<product>();
 }
